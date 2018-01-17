b4835558313d4417da18c9416720b0fcf2b99be7
010786 <wangminjun@htsc.com>
2018-01-17 20:12:24 +0800

    remove test

--- assets/Docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['changepassword_3awitholdpassword_3apasswordtype_3acompleted_3a',['changePassword:withOldPassword:passwordType:completed:',['../d0/d5c/interface_h_t_set_password_request_model.html#af5ac9d82bff7f660c4c6ceeaef0adc9e',1,'HTSetPasswordRequestModel']]]
-];
--- assets/Docs/html/search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['requestphonenumberwithclientno_3acontactid_3acompleted_3a',['requestPhoneNumberWithClientNo:contactId:completed:',['../d0/d5c/interface_h_t_set_password_request_model.html#ae089c85752fb8dbe8a4b656fe8d37e56',1,'HTSetPasswordRequestModel']]],
-  ['requestverificationnocompleted_3a',['requestVerificationNoCompleted:',['../d0/d5c/interface_h_t_set_password_request_model.html#abf0b2bc08f669adc864dc7ccac9d1a55',1,'HTSetPasswordRequestModel']]]
-];
